0b7a2bbfb40faf17985768b9d0809291c23603e5
entropy2333 <40735723+entropy2333@users.noreply.github.com>
2020-02-19 23:30:11 +0800

    updated on 2020-02-19

---
+++ points/iterTools.py
@@ -0,0 +1,27 @@
+import itertools
+
+# count = itertools.count(1)
+# for n in count:
+    # print(n)
+    
+
+# cs = itertools.cycle('ABC')
+# for n in cs:
+#     print(n)
+
+# ns = itertools.repeat('A', 3)
+# for n in ns:
+#     print(n)
+
+# count = itertools.count(1)
+# ns = itertools.takewhile(lambda x: x <= 10, count)
+# print(list(ns))
+
+# for c in itertools.chain('ABC', 'XYZ'):
+#     print(c)
+
+# for key, group in itertools.groupby('AAABBBCCAAA'):
+#     print(key, list(group))
+
+# for key, group in itertools.groupby('AaaBBbcCAAa', lambda c: c.upper()):
+#     print(key, list(group))
